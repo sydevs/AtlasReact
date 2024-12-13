c41f0ce13ad3be197d5378d3860f59b4805707fe
Devindra <devindra.payment@gmail.com>
2024-12-13 14:34:37 +1100

    Add comment to widget.tsx

--- src/Widget.tsx
+++ src/Widget.tsx
@@ -2,6 +2,10 @@ import r2wc from '@r2wc/react-to-web-component';
 import App from './App';
 import { HashRouter } from 'react-router';
 
+// Implementation of embeddable Widget
+// Demo in: demo.html
+// Based on: https://www.linkedin.com/pulse/converting-react-app-appendable-widget-using-web-mike-rahimi-wssnf/
+
 export default function Widget({ ...props } : any) {
   console.log('load app', props);
   return (
