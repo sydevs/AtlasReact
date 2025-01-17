5ca07204efe0adf8a5b4fdac59da11f86ec3f382
Devindra <devindra.payment@gmail.com>
2025-01-17 08:40:11 +0000

    Adjust event item spacing for Inactive classes

--- src/components/list/event.tsx
+++ src/components/list/event.tsx
@@ -21,8 +21,8 @@ export default function EventItem({ event }: Props) {
 
   return (
     <Link href={event.path} className="block px-6 text-inherit transition-colors hover:bg-primary-10">
-      <li key={event.id} className="flex flex-row py-5 items-center border-b border-divider">
-        <div className="flex flex-grow flex-col gap-1">
+      <li key={event.id} className="flex-center-y py-5 border-b border-divider min-h-36">
+        <div className="flex flex-grow flex-col gap-1 self-stretch">
           <div className="font-semibold text-lg leading-tight">
             {event.label}
           </div>
@@ -34,15 +34,16 @@ export default function EventItem({ event }: Props) {
               t(`recurrence.${event.recurrenceType}`, { weekday: nextDate.toLocaleString({ weekday: 'long' }) }) :
               t('details.contact_for_timing')}
           </div>
-          <div className="text-xs text-gray-500">
-            <EventTime
-              nextDate={nextDate}
-              duration={event.duration}
-              timeZone={event.online ? DateTime.local().zoneName : event.timeZone}
-              showTimeZone={event.online}
-              delay={500}
-            />
-          </div>
+          {event.recurrenceType &&
+            <div className="text-xs text-gray-500">
+              <EventTime
+                nextDate={nextDate}
+                duration={event.duration}
+                timeZone={event.online ? DateTime.local().zoneName : event.timeZone}
+                showTimeZone={event.online}
+                delay={500}
+              />
+            </div>}
           <div className="flex items-center mt-1 gap-1">
             {event.firstDate &&
               <EventSoonChip firstDate={event.firstDate} online={event.online} />}
